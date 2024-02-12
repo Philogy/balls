30a119ffce4f8286c9076d1cdd718646b7e28019
Philippe Dumonet <philippe@dumo.net>
2024-02-12 20:42:45 +0000

    🥢 Add time overview

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use balls::scheduling::astar::AStarScheduler;
 use balls::scheduling::dijkstra::Dijkstra;
 use balls::scheduling::{BackwardsMachine, Step};
 use balls::transformer::GlobalContext;
+use std::time::Instant;
 
 fn main() {
     let file_path = std::env::args().nth(1).unwrap();
     let src = std::fs::read_to_string(&file_path).unwrap();
 
+    let start = Instant::now();
     let lex_out = lexer::lex(src.as_str());
 
     // TODO: Proper lexer error handling
@@ -25,34 +27,49 @@ fn main() {
     if errored {
         std::process::exit(1);
     }
+    let parse_lex_time = start.elapsed().as_micros() as f64 / 1000.0;
 
     if let Some(ast_nodes) = maybe_ast_nodes {
+        let start = Instant::now();
         let ctx = GlobalContext::from(ast_nodes);
 
-        for macro_def in ctx.macros.iter() {
-            println!("Macro {:?}", macro_def.name);
+        let macro_def = ctx.macros.first().expect("No macro to schedule");
+        println!("Macro {:?}", macro_def.name);
 
-            let tmacro = ctx.transform(macro_def.clone());
+        let tmacro = ctx.transform(macro_def.clone());
 
-            let machine: BackwardsMachine = tmacro.clone().into();
+        let machine: BackwardsMachine = tmacro.clone().into();
+        let preprocessing_time = start.elapsed().as_micros() as f64 / 1000.0;
 
-            let (total, cost, steps) = Dijkstra::schedule(machine);
+        let start = Instant::now();
+        let (total, cost, steps) = Dijkstra::schedule(machine);
+        let schedule_time = start.elapsed().as_secs_f64();
 
-            for step in steps {
-                let s = match step {
-                    Step::Op(id) => match &tmacro.nodes[id].1 {
-                        Computation::Op(ident) => ident.clone(),
-                        Computation::TopLevelInput(ident) => ident.clone(),
-                        Computation::Const(num) => format!("0x{:x}", num),
-                    },
-                    Step::Dup(depth) => format!("dup{}", depth),
-                    Step::Swap(depth) => format!("swap{}", depth),
-                    Step::Pop => "pop".to_string(),
-                };
-                println!("{}", s);
-            }
-            println!("total: {total}");
-            println!("cost: {cost}");
+        for step in steps {
+            let s = match step {
+                Step::Op(id) => match &tmacro.nodes[id].1 {
+                    Computation::Op(ident) => ident.clone(),
+                    Computation::TopLevelInput(ident) => ident.clone(),
+                    Computation::Const(num) => format!("0x{:x}", num),
+                },
+                Step::Dup(depth) => format!("dup{}", depth),
+                Step::Swap(depth) => format!("swap{}", depth),
+                Step::Pop => "pop".to_string(),
+            };
+            println!("  {}", s);
         }
+        println!();
+        if ctx.macros.len() > 1 {
+            println!("WARNING: More than 1 macro found, only scheduling one at a time for now");
+        }
+        println!("Lexing + parsing: {:.2} ms", parse_lex_time);
+        println!("Macro pre-processing: {:.2} ms", preprocessing_time);
+        println!("\nScheduling: {:.3} s", schedule_time);
+        println!(
+            "explored: {} ({:.0} / s)",
+            total,
+            total as f64 / schedule_time
+        );
+        println!("cost: {}", cost);
     }
 }
